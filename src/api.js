f8db191aad2df9f96fbadbd89b2abb9c972d768e
Sapna <sapnabolikal@gmail.com>
2023-12-13 21:52:00 -0500

    update api url

--- src/api.js
+++ src/api.js
@@ -90,7 +90,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url =  "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api" + "/" + token;
+    const url =  "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-events" + "/" + token;
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
