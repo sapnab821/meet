104700fc2c735f30ef1fd15c5b0265cd19319ad9
Sapna <sapnabolikal@gmail.com>
2023-12-13 22:21:44 -0500

    update api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,4 @@
-// src/api.js
-
 import mockData from './mock-data';
-
 
 /**
  *
@@ -11,8 +8,6 @@ import mockData from './mock-data';
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
- 
-
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
@@ -32,30 +27,39 @@ const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
     newurl =
       window.location.protocol +
-      "//" +
+      '//' +
       window.location.host +
       window.location.pathname;
-    window.history.pushState("", "", newurl);
+    window.history.pushState('', '', newurl);
   } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
   }
 };
 
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    'https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/token' +
+      '/' +
+      encodeCode
+  );
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem('access_token', access_token);
 
+  return access_token;
+};
 
 export const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
-
   const tokenCheck = accessToken && (await checkToken(accessToken));
-
   if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
+    await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
+    const code = await searchParams.get('code');
     if (!code) {
       const response = await fetch(
-        "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
+        'https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
       );
       const result = await response.json();
       const { authUrl } = result;
@@ -63,40 +67,39 @@ export const getAccessToken = async () => {
     }
     return code && getToken(code);
   }
+
   return accessToken;
 };
+
 /**
  *
  * This function will fetch the list of all events
- 
-*/
-
-
+ */
 export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    return events ? JSON.parse(events) : [];
   }
 
   const token = await getAccessToken();
 
   if (token) {
     removeQuery();
-    const url =  "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-events/" + token;
-    const response = await fetch(url);
-    const result = await response.json();
-    if (result) {
-      return result.events;
-    } else return null; 
-  };
-}
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/token/" + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
+    const url =
+      'https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-events' +
+      '/' +
+      token;
+      
+      const response = await fetch(url);
+      const result = await response.json();
+      if (result) {
+        localStorage.setItem('lastEvents', JSON.stringify(result.events));
+        return result.events;
+      } else return null;
+    
+  }
 };
