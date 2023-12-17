db1b4cd04af3a727e45ad493d2a7c94b0cfbc297
Sapna <sapnabolikal@gmail.com>
2023-12-17 16:01:30 -0500

    api

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import mockData from './mock-data';
 
 /**
  *
-  
+  @param {*} events:
  * The following function should be in the “api.js” file.
  * This function takes an events array, then uses map to create a new array with only locations.
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
