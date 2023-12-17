896a0edb1cd954353f5e1f73bc2609c4445a7bef
Sapna <sapnabolikal@gmail.com>
2023-12-17 15:57:57 -0500

    api

--- src/api.js
+++ src/api.js
@@ -12,7 +12,7 @@ import mockData from './mock-data';
  * The Set will remove all duplicates from the array.
  */
  
-/*
+
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
@@ -71,7 +71,7 @@ export const getAccessToken = async () => {
  
 */
 
-/*
+
 export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
@@ -91,138 +91,3 @@ export const getEvents = async () => {
 }
 
 
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(
-    "https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/token/" + encodeCode
-  );
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem("access_token", access_token);
-
-  return access_token;
-};
-
-
-const getToken = async (code) => {
-  try {
-    const encodeCode = encodeURIComponent(code);
- 
-    const response = await fetch( 'https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/token' + '/' + encodeCode);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem("access_token", access_token);
-    return access_token;
-  } catch (error) {
-    error.json();
-  }
- }
-*/
-
-export const extractLocations = async(eventsPromise) => {
-  const events = eventsPromise;
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
-  return locations;
-};
-
-export const getAccessToken = async () => {
-  const accessToken = localStorage.getItem('access_token');
-
-  const tokenCheck = accessToken && (await checkToken(accessToken))
-
-  if(!accessToken || tokenCheck.error) {
-      await localStorage.removeItem('access_token');
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = await searchParams.get('code');
-      const getAuthURL = 'https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url';
-      if (!code) {
-          const response = await fetch(getAuthURL);
-          const result = await response.json();
-          const { authUrl } = result;
-          return (window.location.href = authUrl);
-      }
-      return code && getToken(code);
-  }
-  return accessToken;
-};
-
-const checkToken = async (accessToken) => {
-  try{
-      const response = await fetch(
-          `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-      );
-      const result = await response.json();
-      return result;
-  } catch (error) {
-      console.error('Error checking token', error);
-      return { error: 'Failed to check token'};
-  }
-
-};
-
-const getToken = async (code) => {
-  const encodeCode = encodeURIComponent(code);
-  const response = await fetch(`https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`);
-  const { access_token } = await response.json();
-  access_token && localStorage.setItem('access_token', access_token);
-
-  return access_token;
-};
-
-export const getTestEvents = async () => {
-  return mockData;
-}
-
-/*
-*   FETCH:      data
-*   FROM:       testEventData.js
-*/
-export const getEvents = async () => {
-  
-  if(window.location.href.startsWith('http://localhost')) {
-      return testEventData;
-  }
-
-  if(!navigator.onLine) {
-      const events = localStorage.getItem('offlineEvents');
-      return events?JSON.parse(events):[]
-  }
-  
-  const token = await getAccessToken();
-
-  if(token) {
-      removeQuery();
-      const response = await fetch(`https://owtv818248.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`);
-      if(!response.ok) {
-          console.error('Error while fetching events:', response.statusText);
-          return null;
-      }        
-      const result = await response.json();
-      if(result) {
-          localStorage.setItem("offlineEvents", JSON.stringify(result.events));
-          return result.events;
-      }else{
-          return null;
-      }
-  }
-};
-
-const removeQuery = () => {
-  let newURL;
-  if(window.history.pushState && window.location.pathname) {
-      newURL = 
-          window.location.protocol + 
-          '//' + 
-          window.location.host +
-          window.location.pathname;
-      window.history.pushState('', '', newURL);
-  }else{
-      newURL = 
-          window.location.protocol +
-          '//' +
-          window.location.host;
-      window.history.pushState('', '', newURL);
-  }
-};
