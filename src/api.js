cce9e37059193c843a1cb56a2f1dc2597696b7d7
Sapna <sapnabolikal@gmail.com>
2023-12-17 20:00:18 -0500

    api

--- src/api.js
+++ src/api.js
@@ -3,32 +3,6 @@
 import mockData from './mock-data';
 
 
-// src/api.js
-
-import mockData from "./mock-data";
-
-/**
- *
- * @param {*} events:
- * The following function should be in the “api.js” file.
- * This function takes an events array, then uses map to create a new array with only locations.
- * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
- * The Set will remove all duplicates from the array.
- */
-export const extractLocations = (events) => {
-  const extractedLocations = events.map((event) => event.location);
-  const locations = [...new Set(extractedLocations)];
-  return locations;
-};
-
-/**
- *
- * This function will fetch the list of all events
- */
-export const getEvents = async () => {
-  return mockData;
-};
-
 
 /**
  *
@@ -39,7 +13,7 @@ export const getEvents = async () => {
  * The Set will remove all duplicates from the array.
  */
  
-/*
+
 export const extractLocations = (events) => {
   const extractedLocations = events.map((event) => event.location);
   const locations = [...new Set(extractedLocations)];
@@ -98,7 +72,7 @@ export const getAccessToken = async () => {
  
 */
 
-/*
+
 export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
@@ -127,4 +101,3 @@ const getToken = async (code) => {
 
   return access_token;
 };
-*/
