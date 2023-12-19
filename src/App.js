768d3a3c88158864dd7c2d72e9e33bec67646fe2
Sapna <sapnabolikal@gmail.com>
2023-12-19 17:38:39 -0500

    change events

--- src/App.js
+++ src/App.js
@@ -19,11 +19,19 @@ const App = () => {
 
   const fetchData = async () => {
     const allEvents = await getEvents();
+
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+    /*
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
     setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
+    setAllLocations(extractLocations(allEvents));*/
   };
 
 
