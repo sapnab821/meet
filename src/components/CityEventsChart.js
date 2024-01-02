e670e12d999cba07005c436df8f9b837d86786f5
Sapna <sapnabolikal@gmail.com>
2024-01-02 18:25:20 -0500

    city events chart update

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -20,7 +20,7 @@ const CityEventsChart = ({ allLocations, events }) => {
     const getData = () => {
         const data = allLocations.map((location) => {
         const count = events.filter((event) => event.location === location).length
-          const city = location.split(/, | - /)[0]
+          const city = location.split((/, | - /))[0]
           return { city , count  };
         })
         return data;
@@ -34,8 +34,8 @@ const CityEventsChart = ({ allLocations, events }) => {
             margin={{
               top: 20,
               right: 20,
-              bottom: 20,
-              left: 20,
+              bottom: 60,
+              left: -30,
             }}
           >
             <CartesianGrid />
