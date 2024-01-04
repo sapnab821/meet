9e25bdf3a6bc9a409448e4196851e7b1291986ee
Sapna <sapnabolikal@gmail.com>
2024-01-03 19:11:26 -0500

    chart changes

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -40,9 +40,9 @@ const CityEventsChart = ({ allLocations, events }) => {
             }}
           >
             <CartesianGrid />
-            <XAxis type="category" dataKey="city" name="City" 
+            <XAxis type="category" dataKey='city' name='City' 
             angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}/>
-            <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false}/>
+            <YAxis type="number" dataKey='count' name='Number of events' allowDecimals={false}/>
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
             <Scatter name="A school" data={data} fill="#8884d8" />
           </ScatterChart>
